98ea96be33a73a8ccd12155b381318dd63e25611
akshat-tahalani <tahalani.akshat@gmail.com>
2025-08-30 04:33:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 
 options = webdriver.ChromeOptions()
-options.add_argument(r"--user-data-dir=C:\Users\Tahal\AppData\Local\Google\Chrome\User Data\Default")
+options.add_argument(r"--add your chrome deault path profile")
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 driver.get("https://web.whatsapp.com/")
@@ -19,7 +19,7 @@ wait = WebDriverWait(driver, 30)
 search_box = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div[role='textbox'][data-tab='3']")))
 
 search_box.click()
-search_box.send_keys("akki bhaiya")  # Replace with real contact name
+search_box.send_keys("name")  # Replace with real contact name
 
 time.sleep(2)
 
@@ -36,19 +36,15 @@ message_box.send_keys(Keys.ENTER)
 
 time.sleep(5)
 
-print("msh bhej diya ")
+print("msg is sent! ")
 
-#attach_button = driver.find_element(By.XPATH, "//span[@title='Plus rounded']")
-#attach_button.click()
-
-#photos_button = driver.find_element(By.XPATH, "//span[contains(text(), 'Photos & Videos')]")
-#photos_button.click()
 file_input  = driver.find_element(By.CSS_SELECTOR,"input[type='file'][accept*='image']")
 
-image_path = r"C:\Users\Tahal\OneDrive\Pictures\wallpapers\wallhaven-yxwozk_3440x1440.png"
+image_path = r"enter the path of the image to be sent"
 file_input.send_keys(image_path)
 time.sleep(2)
 actions = ActionChains(driver)
 actions.send_keys(Keys.ENTER).perform()
 
 time.sleep(10)
+
